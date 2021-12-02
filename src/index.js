dbcddacce4cf2721804bba377909c015d956a756
BonkeG <bonkegcobo291195@gmail.com>
2021-12-02 14:01:19 +0200

    Make list appear in the order of index values

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    //Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
+const taskList = document.querySelector('.List-Task');
+const renew = document.getElementById('renew');
+
+let toDoList = [{
+  index: 1,
+  description: "Attend the morning standUp",
+  completed: false,
+  }, 
+  {
+    index: 0,
+    description: "Meeting with learning partner",
+    completed: false,
   }
-  
-  document.body.appendChild(component());
+];
+
+toDoList.sort(function(a,b){
+  return a.index-b.index
+});
+
+window.onload = () => {
+  toDoList.forEach((item) => {
+    const listVal =  `<div class="toDo">
+    <div class="FirstTwo">
+      <input class="box" type="checkbox" id="toDo#1" name="toDo#1">
+      <label class="label" for="toDo#1">${item.description}</label><br>
+    </div>
+    <span id="icon" class="material-icons move-icon">more_vert</span>
+  </div>
+  <hr>`; 
+  taskList.insertAdjacentHTML('beforeend', listVal);
+  });
+}
+
+
+
